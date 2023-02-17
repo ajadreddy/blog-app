37e724aae4b3ff0ae40edbd8c48cf7b71d2da845
ajadreddy007 <ajad.edavelly@testaing.com>
2023-02-17 09:08:38 +0530

    Setting up navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/createpost">Create Post</Link>
+          <Link to="/login">Login</Link>
+        </nav>
+        <Routes>
+          <Route path = "/" element={<Home/>}/>
+          <Route path = "/createpost" element={<CreatePost/>}/>
+          <Route path = "/login" element={<Login/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
